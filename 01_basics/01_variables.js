011c1cb16bc5e477cecd8e3eabc4391fe58daea9
Piyush0bhati <114479451+Piyush0bhati@users.noreply.github.com>
2025-04-22 04:10:17 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 12342
+let accountEmail = "pbaeiou@gmail.com"
+var accontPassword = "12345"
+accountCity = "jaipur"
+let.accountState;
+
+// accountId = 2
+
+/*
+prefer not to use var 
+because of issue in block space and functional scope
+*/
+
+accountEmail = "heitch@gmail.com"
+accontPassword = "16524"
+accountCity = "delhi"
+
+
+console.log(accountId);
+
+console.table([accountId,accontPassword,accountEmail,accountCity,accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,7 @@
+"use strict"; 
+
+// alert("hello")
+
+let name = "piyush"
+let age = 23
+let IsLoggedin = false
